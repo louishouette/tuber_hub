57993c958dce6486cbfc98b5e7ba5aa680e56617
Louis Houette <louishouette@gmail.com>
2025-03-12 21:40:58 +0100

    feat: updated the profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Generate initials from a user's name
+  # @param name [String] the full name
+  # @return [String] the initials
+  def user_initials(name)
+    return "" if name.blank?
+    
+    name.split.map(&:first).join
+  end
 end

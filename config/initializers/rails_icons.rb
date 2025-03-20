489cb854368e98b82dcdf49092c45963480b1c25
Louis Houette <louishouette@gmail.com>
2025-03-20 14:19:22 +0100

    chore: changed default icon library

--- config/initializers/rails_icons.rb
+++ config/initializers/rails_icons.rb
@@ -1,5 +1,5 @@
 RailsIcons.configure do |config|
-  config.default_library = "lucide"
+  config.default_library = "heroicons"
   # config.default_variant = "" # Set a default variant for all libraries
 
   # Override Lucide defaults
@@ -8,4 +8,20 @@ RailsIcons.configure do |config|
   # config.libraries.lucide.outline.default.css = "size-6"
   # config.libraries.lucide.outline.default.stroke_width = "1.5"
   # config.libraries.lucide.outline.default.data = {}
+
+  # Override Heroicon defaults
+  # config.libraries.heroicons.exclude_variants = [:mini, :micro] # Exclude specific variants
+
+  # config.libraries.heroicons.outline.default.css = "size-6"
+  # config.libraries.heroicons.outline.default.stroke_width = "1.5"
+  # config.libraries.heroicons.outline.default.data = {}
+
+  # config.libraries.heroicons.solid.default.css = "size-6"
+  # config.libraries.heroicons.solid.default.data = {}
+
+  # config.libraries.heroicons.mini.default.css = "size-5"
+  # config.libraries.heroicons.mini.default.data = {}
+
+  # config.libraries.heroicons.micro.default.css = "size-4"
+  # config.libraries.heroicons.micro.default.data = {}
 end
